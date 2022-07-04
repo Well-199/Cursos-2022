ad4d12d61f9fd59f7510a472119fa4ac06efd5e0
Well-appedidos <wellington.lira@appedidos.com.br>
2022-07-04 14:20:18 -0300

    0.0.1

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,11 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-const reduceResult = numbers.reduce((accumulator, currentValue, currentIndex, array) => {
-    return accumulator + currentValue
-}, 0)
-
-console.log(reduceResult)
 
 
+const fruits = ["Apples", "Bananas", "Grapes", "Oranges"]
 
+const groceriesList = fruits.reduce((accumulator, currentFruit, index) => {
+    return accumulator + `${index+1} - ${currentFruit} \n`
+}, "")
+
+console.log(groceriesList)
 
 
